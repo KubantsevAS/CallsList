530de2225820ed258f08151f2f525d535178ed0d
KubantsevAS <veng93@yandex.ru>
2023-03-02 22:10:13 +0300

    added date sorting

--- src/components/CallsSection/Calls/Calendar/Calendar.jsx
+++ src/components/CallsSection/Calls/Calendar/Calendar.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Calendar.module.css'
 import {CalendarSvg, ArrowLeftSvg, ArrowRightSvg, PlusBtnSvg} from './../../../../images/SvgCollectorForFilter'
+import moment from 'moment/moment';
 
+export default function Calendar (props) {
 
-export default function Calendar () {
+    const dateCal = ['3 дня', 'неделя', 'месяц', 'год']
+    const [dateSelec, setDateSelec] =  useState('3 дня');
+
+    // Функция для присваивания нового значение даты старта
+    function showDateRange (dateSelec) {
+        switch (dateSelec) {
+            case '3 дня' :
+                return props.setDefStart(moment().subtract(2, 'days').format('YYYY-MM-DD'));
+            case 'неделя' :
+                return props.setDefStart(moment().startOf('week').add(1, 'days').format('YYYY-MM-DD'));
+            case 'месяц' :
+                return props.setDefStart(moment().startOf('months').format('YYYY-MM-DD'));
+            case 'год' :
+                return props.setDefStart(moment().startOf('years').format('YYYY-MM-DD'));
+            default: 
+                break;
+        }
+    }
+
+    function onDateIncrement () {
+        if (dateSelec === 'год') {
+            showDateRange (dateSelec);
+            return
+        } else {
+            setDateSelec(dateCal[dateCal.indexOf(dateSelec) + 1]);
+            showDateRange (dateSelec);      
+        }
+    }
+
+    function onDateDecrement () {
+        if (dateSelec === '3 дня') {
+            showDateRange (dateSelec);
+            return 
+        } else {
+            setDateSelec(dateCal[dateCal.indexOf(dateSelec) - 1]);
+            showDateRange (dateSelec);
+        }
+    }
+
     return (
         <div className={styles.calendarContainer}>
 
@@ -13,14 +53,14 @@ export default function Calendar () {
             </span>
 
             <span className={styles.selector}>
-                <div className={styles.arrow + ' ' + styles.left}>
+                <div className={styles.arrow + ' ' + styles.left} onClick={onDateDecrement}>
                     {ArrowLeftSvg()}
                 </div>
                 <div className={styles.days}>
                     {CalendarSvg()}
-                    <span>3 дня</span>
+                    <span>{dateSelec}</span>
                 </div>
-                <div className={styles.arrow + ' ' + styles.right}>
+                <div className={styles.arrow + ' ' + styles.right} onClick={onDateIncrement}>
                     {ArrowRightSvg()}
                 </div>
             </span>
--- src/components/CallsSection/Calls/CallsContainer.jsx
+++ src/components/CallsSection/Calls/CallsContainer.jsx
@@ -4,25 +4,35 @@ import Calendar from './Calendar/Calendar';
 import styles from './CallContainer.module.css'
 import CallElem from './CallElem/CallElem';
 import SearchAndFilters from './SearchAndFilters/SearchAndFilters';
+import moment from 'moment';
 
 const CallsContainer = () => {
+
+    const [defStart, setDefStart] = useState(moment().subtract(2, 'days').format("YYYY-MM-DD"));
+    const [defEnd, setDefEnd] = useState(moment().format("YYYY-MM-DD"));
+    let in_out;
 
     const [calls, setCalls] = useState();
     const [loading, setLoading] = useState(false);
 
+
+    // Получаем данные
+    
     useEffect(() => {
         async function fetchData() {
-            const request = await getCallsData();
+            const request = await getCallsData(defStart, defEnd, in_out);
             setCalls(request.data.results);
             setLoading(true);
         }
         fetchData();
-    }, []);
+    }, [defStart, defEnd, in_out]);
+    
     console.log(calls)
+    console.log(defStart)
 
     return (
         <div className={styles.container}>
-            <Calendar />
+            <Calendar setDefEnd={setDefEnd} setDefStart={setDefStart} defStart={defStart} setCalls={setCalls}/>
             <SearchAndFilters/>
             <div className={styles.elementsField}>
                 <div></div>
