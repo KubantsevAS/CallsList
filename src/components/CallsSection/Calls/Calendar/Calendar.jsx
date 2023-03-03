3658b2382a9712f1f68deda3b7e8be885c54adc2
KubantsevAS <veng93@yandex.ru>
2023-03-03 03:20:33 +0300

    refactorind date sorting, added styles

--- src/components/CallsSection/Calls/Calendar/Calendar.jsx
+++ src/components/CallsSection/Calls/Calendar/Calendar.jsx
@@ -1,46 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState, useRef } from 'react';
 import styles from './Calendar.module.css'
-import {CalendarSvg, ArrowLeftSvg, ArrowRightSvg, PlusBtnSvg} from './../../../../images/SvgCollectorForFilter'
+import { CalendarSvg, ArrowLeftSvg, ArrowRightSvg, PlusBtnSvg } from './../../../../images/SvgCollectorForFilter'
 import moment from 'moment/moment';
 
-export default function Calendar (props) {
+export default function Calendar(props) {
 
-    const dateCal = ['3 дня', 'неделя', 'месяц', 'год']
-    const [dateSelec, setDateSelec] =  useState('3 дня');
+    const catMenu = useRef(null)
+
+    const dateCal = ['3 дня', 'Неделя', 'Месяц', 'Год']
+    const [dateSelec, setDateSelec] = useState('3 дня');
+    const [menuActive, setMenuActive] = useState(false);
+
+    const closeOpenMenus = (e)=>{
+        if(catMenu.current && menuActive && !catMenu.current.contains(e.target)){
+            setMenuActive(false)
+        }
+    }
+    document.addEventListener('mousedown', closeOpenMenus)
+
 
     // Функция для присваивания нового значение даты старта
-    function showDateRange (dateSelec) {
+    function showDateRange(dateSelec) {
         switch (dateSelec) {
-            case '3 дня' :
+            case '3 дня':
                 return props.setDefStart(moment().subtract(2, 'days').format('YYYY-MM-DD'));
-            case 'неделя' :
+            case 'Неделя':
                 return props.setDefStart(moment().startOf('week').add(1, 'days').format('YYYY-MM-DD'));
-            case 'месяц' :
+            case 'Месяц':
                 return props.setDefStart(moment().startOf('months').format('YYYY-MM-DD'));
-            case 'год' :
+            case 'Год':
                 return props.setDefStart(moment().startOf('years').format('YYYY-MM-DD'));
-            default: 
+            default:
                 break;
         }
     }
 
-    function onDateIncrement () {
-        if (dateSelec === 'год') {
-            showDateRange (dateSelec);
+    function onDateIncrement() {
+        if (dateSelec === 'Год') {
+            showDateRange(dateSelec);
             return
         } else {
             setDateSelec(dateCal[dateCal.indexOf(dateSelec) + 1]);
-            showDateRange (dateSelec);      
+            showDateRange(dateSelec);
         }
     }
 
-    function onDateDecrement () {
+    function onDateDecrement() {
         if (dateSelec === '3 дня') {
-            showDateRange (dateSelec);
-            return 
+            showDateRange(dateSelec);
+            return
         } else {
             setDateSelec(dateCal[dateCal.indexOf(dateSelec) - 1]);
-            showDateRange (dateSelec);
+            showDateRange(dateSelec);
         }
     }
 
@@ -56,15 +67,41 @@ export default function Calendar (props) {
                 <div className={styles.arrow + ' ' + styles.left} onClick={onDateDecrement}>
                     {ArrowLeftSvg()}
                 </div>
-                <div className={styles.days}>
+                <div className={styles.days} 
+                    onClick={() => menuActive ? setMenuActive(false) : setMenuActive(true)}
+                    onBlur={() => setMenuActive(false)}
+                    onFocus={() => setMenuActive(false)}
+                >
                     {CalendarSvg()}
                     <span>{dateSelec}</span>
                 </div>
                 <div className={styles.arrow + ' ' + styles.right} onClick={onDateIncrement}>
                     {ArrowRightSvg()}
                 </div>
+
+                {menuActive &&
+                    <div className={styles.dateMenu} ref={catMenu}>
+                        <div className={styles.menuItem + ' ' + (dateSelec === '3 дня' ? styles.menuActive : null)}>
+                            <div>3 дня</div>
+                        </div>
+                        <div className={styles.menuItem}>
+                            <div>Неделя</div>
+                        </div>
+                        <div className={styles.menuItem}>
+                            <div>Месяц</div>
+                        </div>
+                        <div className={styles.menuItem}>
+                            <div>Год</div>
+                        </div>
+                        <div className={styles.menuItem}>
+                            <div>Указать дату</div>
+                        </div>
+                    </div>
+                }
             </span>
-            
+
+
+
         </div>
     )
 }
--- src/components/CallsSection/Calls/CallElem/CallElem.jsx
+++ src/components/CallsSection/Calls/CallElem/CallElem.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
+import { IncomingSvg, OutgoingSvg } from '../../../../images/SvgCollectorCallElem'
 import styles from './CallElem.module.css'
+import defAva from './../../../../images/defAva.png'
 
 export default function CallElem(props) {
+
+    let phone = props.partner_data.phone;
+
+    let time = `${Math.floor(props.time / 60)}:${props.time % 60 < 10 ? '0' + props.time % 60 : props.time % 60}`
     return (
         <div>
             <div className={styles.callElem}>
-                <span>{props.in_out}</span>
-                <span>
+                <span className={!props.time && styles.incomeSvg}>{props.in_out ? IncomingSvg() : OutgoingSvg()}</span>
+                <span className={styles.callTime}>
                     {props.date.split(' ')[1].split(':').slice(0, 2).join(':')}
                 </span>
-                <span>
-                    <img src={props.person_avatar} alt='no data'></img>
+                <span className={styles.avatar}>
+                    <img src={props.person_avatar || defAva} alt='no data'></img>
                 </span>
-                <span>
+                <span className={styles.partnerInfo}>
                     <div>{props.partner_data.name}</div>
-                    <div>{props.partner_data.phone}</div>
+                    <div>{`+${phone[0]} (${phone.slice(1, 4)}) ${phone.slice(4, 7)}-${phone.slice(7, 9)}-${phone.slice(9, 11)}`}</div>
                 </span>
-                <span>{props.source}источник</span>
-                <span>оценка</span>
-                <span>{props.time}</span>
+                <span className={styles.source}>{props.source}</span>
+                <span className={styles.mark}>оценка</span>
+                <span className={styles.record}>{props.time ? time : null}</span>
             </div>
         </div>
     )
